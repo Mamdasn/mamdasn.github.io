dfef6ea4c3e419133444a3a019e72423de52f282
mamdasn <mhsaber1375@gmail.com>
2021-07-09 16:47:05 +0430

    Updated base

--- base/put-blogs-into-index-html.py
+++ base/put-blogs-into-index-html.py
@@ -18,10 +18,12 @@ blog_txts = ''
 for blog in blogs:
     print(blog)
     with open(f"{blogs_folder}{slash}{blog}", 'r') as f:
-        modified_date=time.strftime('%a %H:%M %B %d, %Y', time.localtime(os.path.getmtime(f"{blogs_folder}{slash}{blog}")))
-        blog_txt = f"{f.read()}\n \
+        # modified_date=time.strftime('%a %H:%M %B %d, %Y', time.localtime(os.path.getmtime(f"{blogs_folder}{slash}{blog}")))
+        blog_txt_raw  = f.readlines()
+        modified_date = blog_txt_raw[-1]
+        blog_txt = f"{''.join(blog_txt_raw[:-1])}\n \
                       <div class='time'>{modified_date}</div>\n \
-                      <div class='horizontalline'></div>"
+                      <div class='stylishhorizontalline'></div>"
         blog_txts = f"{blog_txts}\n{blog_txt}"
 
 html_base_file = html_base_file.replace('<!-- [] -->', f"{blog_txts}")
